a25eb14e32a60a89b77be6e67b90e955325a6306
shubham-pawar9 <developer.shubhampawar@gmail.com>
2023-11-18 23:00:45 +0530

    update some css for mobile

--- src/component/Header.js
+++ src/component/Header.js
@@ -43,7 +43,7 @@ const Header = ({
                   <li
                     id={item.replace(/\s+/g, "")}
                     onClick={(e) => {
-                      handleSelectCategory(e);
+                      handleSelectCategory(e.target.id);
                     }}
                   >
                     {item}
